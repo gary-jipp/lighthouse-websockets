c6019a543fa97b7162b4c98f8c7d5d5dcc647cdf
Gary J <github@accu.ca>
2022-04-11 15:44:58 -0700

    Update

---
+++ express-jquery/public/scripts/email.js
@@ -0,0 +1,9 @@
+// Generates a random email address
+function randomEmail(size) {
+  const chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+  let string = chars[Math.floor(Math.random() * 26)];
+  for (let i = 0; i < size - 1; i++) {
+    string += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return (string + '@gmail.com');
+};
--- express-jquery/public/scripts/index.js
+++ express-jquery/public/scripts/index.js
@@ -3,13 +3,3 @@ const email = randomEmail(3);
 $(function() {
   $(".email").text(email);
 });
-
-// Generates a random email address
-function randomEmail(size) {
-  const chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
-  let string = chars[Math.floor(Math.random() * 26)];
-  for (let i = 0; i < size - 1; i++) {
-    string += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return (string + '@gmail.com');
-};

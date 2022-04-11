9ebc387bc75bcba91c81b42652570077564cca90
Gary J <github@accu.ca>
2022-04-11 15:47:58 -0700

    Update

--- express-jquery/public/scripts/index.js
+++ express-jquery/public/scripts/index.js
@@ -69,13 +69,3 @@ const sendMessage = function(socket) {
   // defaults to "message" event
   // socket.send({ to, text });
 };
-
-// Generates a random email address
-function randomEmail(size) {
-  const chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
-  let string = chars[Math.floor(Math.random() * 26)];
-  for (let i = 0; i < size - 1; i++) {
-    string += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return (string + '@gmail.com');
-};

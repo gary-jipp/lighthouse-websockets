eb9c179ad591a78c20c0d947f7868e8fe0c64954
Gary J <github@accu.ca>
2022-06-06 23:52:28 -0700

    Update

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,19 +1,12 @@
-import { useEffect, useState } from 'react';
-import randomEmail from 'email';
 import 'App.css';
 
 export default function App() {
-  const [email, setEmail] = useState();
 
-  // This is our login email
-  useEffect(() => {
-    setEmail(randomEmail(3));
-  }, []);
 
   return (
     <div className="App">
       <h1>Web Sockets React</h1>
-      <div className="email">{email}</div>
+      <div className="name">{"My Name"}</div>
     </div >
   );
 }

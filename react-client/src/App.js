702bf9af8b6ecc3837b470634488310f1d121c1b
Gary J <github@accu.ca>
2022-08-01 13:55:39 -0700

    Update

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -2,13 +2,13 @@ import 'App.css';
 import {useState} from 'react';
 
 export default function App() {
-  const name = useState("Hello World");
+  const text = useState("Hello World");
 
 
   return (
     <div className="App">
       <h1>Web Sockets</h1>
-      <div className="name">{name}</div>
+      <div className="name">{text}</div>
     </div >
   );
 }

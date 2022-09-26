17604d61a3004bc984d113507acd790faa07c2ec
Gary J <github@accu.ca>
2022-09-25 22:14:15 -0700

    Update

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,12 +1,14 @@
 import Chat from 'Chat';
 import 'App.css';
+import {useState} from 'react';
 
 export default function App() {
+  const [auth, setAuth] = useState(true);
 
   return (
     <div className="App">
       <h1>Web Sockets React</h1>
-      <Chat />
+      {auth && <Chat />}
     </div >
   );
 }

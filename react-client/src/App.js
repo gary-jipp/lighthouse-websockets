ac258593641271073bfbdf46c900e67261d31059
Gary J <github@accu.ca>
2022-09-25 21:35:04 -0700

    Update

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,12 +1,68 @@
-import Chat from 'Chat';
+import {useEffect, useState} from 'react';
+import io from "socket.io-client";
 import 'App.css';
 
 export default function App() {
+  const [name, setName] = useState("Hello World");
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState("");
+  const [to, setTo] = useState("");
+  const [socket, setSocket] = useState();
+
+  useEffect(() => {
+    // Connect to server
+    const socket = io();
+    setSocket(socket);
+
+    socket.on('connect', () => {
+      console.log("Connected!");
+    });
+
+    socket.on('user', msg => {
+      setMessages(prev => [`${msg.from} says:" ${msg.text}`, ...prev,]);
+    });
+
+    socket.on('server', msg => {
+      setMessages(prev => [msg, ...prev,]);
+    });
+
+    socket.on('name', (data) => {
+      setName(data);
+    });
+
+    return () => socket.disconnect(); // prevents memory leak!
+  }, []);
+
+  const list = messages.map(msg => <li>{msg}</li>);
+
+  const send = function() {
+    socket.emit("message", {to, text});
+  };
 
   return (
     <div className="App">
       <h1>Web Sockets React</h1>
-      <Chat />
+      <div className="email">{name}</div>
+
+      <div>
+        <input
+          onChange={event => setTo(event.target.value)}
+          value={to}
+          placeholder="Recipient" />
+      </div>
+
+      <div>
+        <textarea
+          onChange={e => setText(e.target.value)}
+          placeholder="Type a message" />
+      </div>
+      <button onClick={send} >Send</button>
+
+      <button onClick={() => setMessages([])}>Clear</button>
+      <ul>
+        {list}
+      </ul>
+
     </div >
   );
 }

4df18b335c44279038f5e8aea0041c5bf0938b14
gary <github@accu.ca>
2023-06-05 22:24:59 -0700

    Update packages

--- react-client/src/pages/Login.js
+++ react-client/src/pages/Login.js
@@ -4,14 +4,14 @@ export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const login = function(event) {
+  const onSubmit = function(event) {
     event.preventDefault();
     email && props.login(email, password);
   };
 
   return (
     <div className="login">
-      <form onSubmit={login}>
+      <form onSubmit={onSubmit}>
         <p>
           <input type="text" name="username"
             value={email} placeholder="Enter Username or email"
